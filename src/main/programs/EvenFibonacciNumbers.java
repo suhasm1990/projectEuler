eca375035a0f3225596a2daa14eeb1363672cd67
Suhas Mallesh <suhasmallesh@gmail.com>
2019-04-04 15:57:57 -0500

    Program 2

---
+++ src/main/programs/EvenFibonacciNumbers.java
@@ -0,0 +1,30 @@
+public class EvenFibonacciNumbers {
+
+    public static void main(String[] args){
+        System.out.println(sumEvenFibonacciNumbers(4000000));
+    }
+
+    public static int sumEvenFibonacciNumbers(int numberLimit){
+
+        int finalSum=0;
+        int evenSum=0;
+        int term1=0;
+        int term2=1;
+        int tempSum=0;
+
+        //tempSum is to make sure that finalSum doesn't exist the number limit, else the finalSum will exceed number limit onceßß
+        while(tempSum <= numberLimit){
+            finalSum = term1 + term2;
+            term1=term2;
+            term2=finalSum;
+            if(finalSum % 2 == 0){
+                evenSum = evenSum + finalSum;
+            }
+            tempSum = term1 + term2;
+        }
+
+        return evenSum;
+
+    }
+
+}
--- src/test/programs/MultiplesOf3and5Test.java
+++ src/test/programs/MultiplesOf3and5Test.java
@@ -4,9 +4,9 @@ import static org.junit.Assert.assertEquals;
 public class MultiplesOf3and5Test {
 
     @Test
-    public void testMultiplesOf3and5(){
-        assertEquals("Test case for Multiples of 3 or 5 below 10", 23, MultiplesOf3and5.sumOfMultiplesOf3and5(10));
-        assertEquals("Test case for Multiples of 3 or 5 below 1000", 233168, MultiplesOf3and5.sumOfMultiplesOf3and5(1000));
+    public void testSumMultiplesOf3and5(){
+        assertEquals("Test case for Sum of Multiples of 3 or 5 below 10", 23, MultiplesOf3and5.sumOfMultiplesOf3and5(10));
+        assertEquals("Test case for Sum of Multiples of 3 or 5 below 1000", 233168, MultiplesOf3and5.sumOfMultiplesOf3and5(1000));
     }
 
 }
